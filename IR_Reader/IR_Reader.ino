5bc33f3ede0131cb68165636f32d4a60da1b012d
Bryan Herrera <engineering.herrera@gmail.com>
2024-10-26 13:17:18 -0700

    remove commented code

--- IR_Reader/IR_Reader.ino
+++ IR_Reader/IR_Reader.ino
@@ -92,25 +92,5 @@ void printpulses(void) {
       Serial.println(",");
     }
   }
-  
-  // print it in a 'array' format
-  // Serial.println("int IRsignal[] = {");
-  // Serial.println("// ON, OFF ");
-  // for (uint8_t i = 0; i < currentpulse-1; i++) {
-  //   //Serial.print("\t"); // tab
-  //   Serial.print("pulseIR(");
-  //   Serial.print(pulses[i][1] * RESOLUTION , DEC);
-  //   Serial.print(");");
-  //   Serial.println("");
-  //   //Serial.print("\t");
-  //   Serial.print("delayMicroseconds(");
-  //   Serial.print(pulses[i+1][0] * RESOLUTION , DEC);
-  //   Serial.println(");");
- 
-  // }
-  // //Serial.print("\t"); // tab
-  // Serial.print("pulseIR(");
-  // Serial.print(pulses[currentpulse-1][1] * RESOLUTION, DEC);
-  // Serial.print(");");
  
 }
